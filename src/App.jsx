db141f83cfc08bfd24eadc21b099c7ddb14b014c
Monique Avila <Mon1qu3@icloud.com>
2024-03-06 12:23:24 +0000

    add checker

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+import PalindromeChecker from './components/Palindrome-Checker'
+
+function App() {
+
+  return (
+    <>
+      <PalindromeChecker />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Palindrome-Checker.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+export default function PalindromeChecker() {
+    const [check, setCheck] = useState('');
+    const [results, setResults] = useState('');
+    
+    const checker = async() => {
+        // To account for spaces we remove them completely and compare against og results(check)
+            const adjustedStr = check.replace(/\s/g, '').toLowerCase()
+            const reverse = check.replace(/\s/g, '').split('').reverse().join('');
+            if(!check){
+                setResults('Please input a value');
+            }else if(reverse === adjustedStr){
+                setResults(`${check} is a palindrome`);
+            }else{
+                setResults(`${check} is not a palindrome`);
+            }
+            setCheck('');
+    }
+
+    return(
+        <>
+            <h1>Check your word below!</h1>
+            <div>
+                <label htmlFor="text-input">See if a word is a palindrome:  
+                </label>
+                <br></br>
+                <input id="text-input" placeholder="cat = false or eye = true" value={check} onChange={(e) => setCheck(e.target.value)} autoComplete="off" />
+                <br></br>
+                <button id="check-btn" onClick={() => checker()}>Find Out!</button>
+            </div>
+            {results && (
+                <div id="result">{results}</div>
+            )}
+        </>
+    )
+}
