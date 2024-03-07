b2fd778d3fd3404ab8dd7ed59e0d9a9a85dfaf80
Monique Avila <Mon1qu3@icloud.com>
2024-03-07 02:38:34 +0000

    Added logic and CSS styling

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # My solution to FreeCodeCamps...  
 ## Palindrome Checker!
 
-A palindrome is a word or phrase that can be read the same way forwards and backwards, ignoring punctuation, case, and spacing.# palindrome_checker
+A palindrome is a word or phrase that can be read the same way forwards and backwards, ignoring punctuation, case, and spacing.
+
+This is project 1/5 for completion towards FreeCodeCamp.org JavaScript Algorithms and Data Strucutres(Beta) Certification.
--- src/components/Palindrome-Checker.jsx
+++ src/components/Palindrome-Checker.jsx
@@ -9,7 +9,7 @@ export default function PalindromeChecker() {
             const adjustedStr = check.replace(/\s/g, '').toLowerCase()
             const reverse = check.replace(/\s/g, '').split('').reverse().join('');
             if(!check){
-                setResults('Please input a value');
+                setResults('Please input a value!');
             }else if(reverse === adjustedStr){
                 setResults(`${check} is a palindrome`);
             }else{
@@ -24,14 +24,18 @@ export default function PalindromeChecker() {
             <div>
                 <label htmlFor="text-input">See if a word is a palindrome:  
                 </label>
-                <br></br>
+                <br></br><br></br>
                 <input id="text-input" placeholder="cat = false or eye = true" value={check} onChange={(e) => setCheck(e.target.value)} autoComplete="off" />
-                <br></br>
+                <br></br><br></br>
                 <button id="check-btn" onClick={() => checker()}>Find Out!</button>
             </div>
             {results && (
                 <div id="result">{results}</div>
             )}
+            <div id='me'>
+                <p><em>Created by: Monique</em></p>
+                <a href="https://github.com/Moniii333/palindrome_checker">Check out my source code!</a>
+            </div>
         </>
     )
 }
